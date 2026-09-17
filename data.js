fba3a27294b3575d0a8f45d4d23b723cc39f9911
isaacnimaro <isaac@example.com>
2026-09-17 21:55:45 +0300

    Replace me.png with Olwa.jpg

---
+++ data.js
@@ -0,0 +1,60 @@
+const clientData = [
+    {
+        name: "Kuttos Fashion Store",
+        folder: "Clients/Kuttos Fashion Store",
+        logo: "Clients/Kuttos Fashion Store/logo.png",
+        files: [
+            "Informative 3.png", "Inhouse video 1b.mp4", "Inhouse video 3.mp4", "VIdeo 13.mp4",
+            "Video 1.mp4", "Video 10.mp4", "Video 11.mp4", "Video 12.mp4", "Video 14.mp4",
+            "Video 15.mp4", "Video 2.mp4", "Video 3.mp4", "Video 4.mp4", "Video 5.mp4",
+            "Video 6.mp4", "Video 7.mp4", "Video 8.mp4", "Video 9.mp4", "Video Idea 4.mp4",
+            "Video Idea 5.mp4", "Video Idea 6.mp4", "Video idea 1.mp4", "Video idea 2.mp4",
+            "conference vid1.mp4", "informative 4.png", "informative project 2.png",
+            "informative.png", "inhouse video 2 final1.mp4", "kuttos blender 3.png",
+            "kuttos blender project 1.png", "project 1 final 4.mp4", "project 2.png"
+        ]
+    },
+    {
+        name: "Timeless Vibes Music",
+        folder: "Clients/Timeless Vibes Music",
+        logo: "Clients/Timeless Vibes Music/logo.png",
+        files: [
+            "0408(2).mp4", "Can't save yourself cover photo.png", "Content 1 final.mp4",
+            "Content 2 final.mp4", "Creezy Oka Vid1.mp4", "Face it cover art final.png",
+            "Tripping final cover art.png", "fly cover art final final i swear.jpg",
+            "logo.png", "remix 4.png", "tvm video.mp4", "video 2.mp4"
+        ]
+    },
+    {
+        name: "Byansi Fashions",
+        folder: "Clients/Byansi Fashions",
+        logo: "Clients/Byansi Fashions/logo.png",
+        files: [
+            "BYANSI FASHIONS.png", "DP.png", "PROJECT 1.png", "PROJECT 10.png", "PROJECT 11 b.png",
+            "PROJECT 11.png", "PROJECT 11 a.png", "PROJECT 12 C.png", "PROJECT 12 d.png",
+            "PROJECT 12.png", "PROJECT 12b.png", "PROJECT 13.png", "PROJECT 14.png",
+            "PROJECT 14b.png", "PROJECT 15.png", "PROJECT 15a.png", "PROJECT 19.png",
+            "PROJECT 2.png", "PROJECT 20.png", "PROJECT 2A.png", "PROJECT 3 B.png",
+            "PROJECT 3.png", "PROJECT 3A.png", "PROJECT 3B.png", "PROJECT 4.png",
+            "PROJECT 4A.png", "PROJECT 5.png", "PROJECT 6.png", "PROJECT 7.png",
+            "PROJECT 7A.png", "PROJECT 7B.png", "PROJECT 8.png", "PROJECT 9.png",
+            "WhatsApp Image 2025-12-28 at 7.00.09 PM.jpeg"
+        ]
+    },
+    {
+        name: "Other Projects (3D)",
+        folder: "Clients/Other Projects (3D)",
+        logo: "Clients/Other Projects (3D)/logo.png",
+        files: [
+            "0001.png", "CPU.png", "Final render.png", "JOY MONOCHROMATIC.png", "JOY.png",
+            "Jovan two.png", "Scene 4b.png", "analogas creezy.png", "bd render 4.png",
+            "blue and green.png", "comp.png", "cooker3.png", "full co,p.png",
+            "informative project 2.png", "jovan one.png", "kitchen 2.png",
+            "kuttos blender 3.png", "kuttos blender project 1.png", "purple and blue.png",
+            "scene 1 a.png", "scene 3.png", "scene 4.png", "scene 4a.png", "scene 5.png",
+            "scene 5a.png", "scene 5b.png", "scene 6.png", "view 3.png", "vintage mic 3.png"
+        ]
+    }
+];
+
+export default clientData;
